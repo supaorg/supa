ee7738d913183cf5265759c54bb4bfd8480a10cd
Dmitry Kury <dima.kurilchenko@gmail.com>
2024-08-29 23:59:25 +0700

    Move texts to shared

--- front/src/lib/stores/txtStore.ts
+++ front/src/lib/stores/txtStore.ts
@@ -1,5 +1,5 @@
-import { createTexts } from "$lib/localization/getTexts";
-import type { Texts } from "$lib/localization/texts";
+import { createTexts } from "@shared/localization/getTexts";
+import type { Texts } from "@shared/localization/texts";
 import { writable } from "svelte/store";
 
 // @TODO: implement inserting a target language
