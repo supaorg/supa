b5f29b262da236f3c3058716b0907a6ba52a1fa4
Dmitry Kury <dima.kurilchenko@gmail.com>
2024-09-27 15:25:33 +0700

    Make nodes draggable

--- src/client/src/lib/comps/trees/TreeBlockViz.svelte
+++ src/client/src/lib/comps/trees/TreeBlockViz.svelte
@@ -7,7 +7,7 @@
 
   let children: TreeNode[] = [];
   let isRoot: boolean;
-  let isExpanded = true;
+  let isExpanded = false;
 
   onMount(() => {
     children = tree.getChildren(nodeId);
@@ -17,23 +17,68 @@
   function toggleExpand() {
     isExpanded = !isExpanded;
   }
+
+  function handleDragStart(event: DragEvent) {
+    console.log(event.target);
+
+    event.dataTransfer?.setData("text/plain", nodeId);
+  }
+
+  function handleDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.dataTransfer!.dropEffect = "move";
+
+    console.log("Dropped on ", event.currentTarget);
+  }
+
+  function handleDrop(event: DragEvent) {
+    event.preventDefault();
+    const draggedNodeId = event.dataTransfer?.getData("text/plain");
+    if (draggedNodeId && draggedNodeId !== nodeId) {
+      console.log("Dropped node:", draggedNodeId, "on", nodeId);
+      //tree.move(draggedNodeId, nodeId);
+      // Refresh the children list
+      //children = tree.getChildren(nodeId);
+    }
+  }
 </script>
 
-<div class="tree-item" class:ml-4={!isRoot}>
+<div
+  class="tree-item"
+  class:ml-4={!isRoot}
+  draggable="true"
+  role="treeitem"
+  tabindex="0"
+  aria-selected="false"
+  on:dragstart={handleDragStart}
+  on:dragover={handleDragOver}
+  on:drop={handleDrop}
+>
   <button
     class="tree-item-summary w-full text-left list-none flex items-center cursor-pointer space-x-4 rounded-container-token py-4 px-4 hover:variant-soft"
     on:click={toggleExpand}
     aria-expanded={isExpanded}
     type="button"
   >
-    <div class="tree-summary-symbol fill-current w-3 text-center transition-transform duration-[200ms]" class:rotate-180={isExpanded}>
+    <div
+      class="tree-summary-symbol fill-current w-3 text-center transition-transform duration-[200ms]"
+      class:rotate-180={isExpanded}
+    >
       {#if children.length > 0}
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path>
+          <path
+            d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
+          ></path>
         </svg>
       {:else}
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-3 opacity-10">
-          <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"></path>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          class="w-3 opacity-10"
+        >
+          <path
+            d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"
+          ></path>
         </svg>
       {/if}
     </div>
