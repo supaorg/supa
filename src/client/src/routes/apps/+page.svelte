505a0eaf754ccbbd42ef5a5008cc47b1f85224bd
Dmitry Kury <dima.kurilchenko@gmail.com>
2024-11-18 23:58:45 +0700

    Add apps page

--- src/client/src/routes/apps/+page.svelte
+++ src/client/src/routes/apps/+page.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
   import App from "$lib/comps/apps/App.svelte";
+  import CenteredPage from "$lib/comps/basic/CenteredPage.svelte";
+  import AppConfigTableCell from "$lib/comps/apps/AppConfigTableCell.svelte";
+  import { txtStore } from "$lib/stores/txtStore";
 </script>
 
-<App>Apps here</App>
+<App>
+  <CenteredPage>
+    <h2 class="h2 pb-6">{$txtStore.appPage.title}</h2>
+    <div class="card p-4 space-y-4 mb-4">
+      <h3 class="h3">Chats</h3>
+      <table class="table-auto w-full">
+        <tbody>
+          {#each $appConfigsStore as config (config.id)}
+            <AppConfigTableCell {config} />
+          {/each}
+        </tbody>
+      </table>
+
+      <a href="/apps/new-config" class="btn mt-2 variant-ringed-primary"
+        >{$txtStore.appPage.buttonNewConfig}</a
+      >
+    </div>
+    <div class="card p-4">
+      An ability to create other types of apps is coming at some point. Write at <a
+        class="anchor"
+        href="mailto:hey@supamind.org">hey@supamind.org</a
+      > if you have ideas or suggestions for an app.
+    </div>
+  </CenteredPage>
+</App>
