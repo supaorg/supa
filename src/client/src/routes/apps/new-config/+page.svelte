1cdb0ab828589d42bc22578aceff9137ccd2a9dc
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-01-06 17:32:00 +0700

    Fix breadcrumbs

--- src/client/src/routes/apps/new-config/+page.svelte
+++ src/client/src/routes/apps/new-config/+page.svelte
@@ -4,11 +4,9 @@
 </script>
 
 <CenteredPage>
-  <ol class="breadcrumb mb-4">
-    <li class="crumb">
-      <a class="anchor" href="/apps">Apps</a>
-    </li>
-    <li class="crumb-separator" aria-hidden="true">&rsaquo;</li>
+  <ol class="flex items-center gap-4 mb-4">
+    <li><a class="opacity-60 hover:underline" href="/apps">Apps</a></li>
+    <li class="opacity-50" aria-hidden="true">&rsaquo;</li>
     <li>New Config</li>
   </ol>
   <AppConfigEditing />
