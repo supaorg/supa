07094cf5fb51a75facb18e30f264f78870400947
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-01-15 16:06:37 +0700

    Make the dev panel nicer

--- src/client/src/lib/comps/dev/DevPanel.svelte
+++ src/client/src/lib/comps/dev/DevPanel.svelte
@@ -3,6 +3,12 @@
   import { spaceInspectorOpen } from "$lib/stores/devMode";
 </script>
 
-<div class="flex gap-4 items-center p-2">
-  <button class="btn btn-sm variant-soft" on:click={() => $spaceInspectorOpen = !$spaceInspectorOpen}>Toggle Inspector</button>
+<div class="flex gap-4 items-center justify-between p-2">
+  <div class="flex items-center gap-2">
+    <div class="text-sm text-surface-600-300-token">🚧 Supa v1.0.0 in Dev Mode</div>
+    <button class="btn btn-sm variant-soft" on:click={() => $isDevMode = false}>Exit Dev Mode</button>
+  </div>
+  <button class="btn btn-sm variant-soft" on:click={() => $spaceInspectorOpen ? ($spaceInspectorOpen = false) : ($spaceInspectorOpen = true)}>
+    {$spaceInspectorOpen ? 'Close Space Inspector' : 'Open Space Inspector'}
+  </button>
 </div>
