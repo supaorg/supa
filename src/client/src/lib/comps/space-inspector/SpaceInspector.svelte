2ac01994fd3e9f21ff912c55940dcd64d72e21ba
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-01-15 23:20:03 +0700

    Make a shortcut for opening tree of an open chat thread

--- src/client/src/lib/comps/space-inspector/SpaceInspector.svelte
+++ src/client/src/lib/comps/space-inspector/SpaceInspector.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+  import { page } from "$app/state";
   import { currentSpaceStore } from "$lib/spaces/spaceStore";
-  import type AppTree from "@shared/spaces/AppTree";
   import VertexView from "./VertexView.svelte";
   import type { Vertex } from "@shared/replicatedTree/Vertex";
 
   let spaceRootVertex = $derived($currentSpaceStore?.rootVertex);
   let appTreeRootVertex = $state<Vertex | undefined>(undefined);
   let showingAppTree = $state(false);
+
+  let currentTreeId = $derived.by(() => {
+    return page.url.searchParams.get("t");
+  });
 
   async function onTreeOpen(treeId: string) {
     const appTree = await $currentSpaceStore?.loadAppTree(treeId);
@@ -31,6 +35,17 @@
       Space
     </button>
   </li>
+  {#if currentTreeId && !showingAppTree}
+    <li class="opacity-50" aria-hidden="true">&rsaquo;</li>
+    <li>
+      <button 
+        class="text-blue-500 hover:underline font-medium"
+        onclick={() => onTreeOpen(currentTreeId)}
+      >
+        Open Current App Tree
+      </button>
+    </li>
+  {/if}
   {#if showingAppTree}
     <li class="opacity-50" aria-hidden="true">&rsaquo;</li>
     <li>App Tree</li>
