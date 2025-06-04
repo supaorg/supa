ec10db56e32f9ed6abb7b0d72f5eb9d8c2c04d45
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-06-04 23:01:11 +0700

    feat(space onboarding): let's load a new space with sidebar closed

--- packages/client/src/lib/ttabs/layout.svelte.ts
+++ packages/client/src/lib/ttabs/layout.svelte.ts
@@ -310,7 +310,7 @@ export function setupDefault(tt: TTabs) {
   tt.resetTiles();
   const root = tt.addGrid();
   const row = tt.addRow(root);
-  layoutRefs.sidebarColumn = tt.addColumn(row, "300px");
+  layoutRefs.sidebarColumn = tt.addColumn(row, "0px");
   tt.setComponent(layoutRefs.sidebarColumn, 'sidebar');
   const parentColumn = tt.addColumn(row);
   layoutRefs.contentGrid = tt.addGrid(parentColumn);
