67ed0644e46b84f9de79163713b3b0292f23cd2b
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-06-09 16:22:28 +0700

    feat(fresh wizard): start the fresh setup screen

--- packages/client/src/lib/comps/wizards/FreshStartWizard.svelte
+++ packages/client/src/lib/comps/wizards/FreshStartWizard.svelte
@@ -4,8 +4,15 @@
 
 <CenteredPage>
   <div class="card p-4 mt-4 space-y-4">
-    <h2 class="h2">welcom to t69.chat</h2>
-    <p class="mb-6">@TODO: add description</p>
-    
+    <h2 class="h2">t69.chat</h2>
+    <p class="mb-6">
+      This is an open-source alternative to <a class="anchor" href="https://t3.chat">t3.chat</a>, a project for a <a class="anchor"
+        href="https://cloneathon.t3.chat/"
+        >hackathon orginized by Theo the youtuber</a
+      >. Here's the <a class="anchor" href="https://github.com/mitkury/t69">source on
+      GitHub</a>.
+    </p>
+    <button class="btn">Sign in/up</button>
+    <button class="btn">Go anon, sign in later</button>
   </div>
 </CenteredPage>
