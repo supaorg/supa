f8f8649f1eefb11e01857bab5be073d7a56343d1
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-06-09 18:09:34 +0700

    feat(fresh wizard): add features

--- packages/client/src/lib/comps/wizards/FreshStartWizard.svelte
+++ packages/client/src/lib/comps/wizards/FreshStartWizard.svelte
@@ -32,6 +32,19 @@
         > (start pls).
       </p>
     </div>
+    <div>
+      <div class="space-y-2">
+        <h3 class="h4">Features:</h3>
+        <ul class="list-disc list-inside space-y-1">
+          <li>Workspaces (or spaces for short)</li>
+          <li>Local first (but you can sync to a server)</li>
+          <li>Bring your own keys for any OpenAI compatible APIs</li>
+          <li>Tabs like in VSCode</li>
+          <li>Many themes</li>
+          <li>Free and open source</li>
+        </ul>
+      </div>
+    </div>
 
     <div class="flex flex-col gap-4 pt-4">
       <div class="flex flex-col gap-2">
