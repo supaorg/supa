ee6750b1f2d3856049cc22e2b8ced002575b8388
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-07-07 18:10:02 +0500

    chore: remove the old SpaceConnection

--- packages/client/src/lib/spaces/SpaceConnection.ts
+++
@@ -1,8 +0,0 @@
-import type Space from "@core/spaces/Space";
-
-export interface SpaceConnection {
-  get space(): Space;
-  get connected(): boolean;
-  connect(): Promise<void>;
-  disconnect(): Promise<void>;
-}
