fb56218f68a0778cbad615fe5993baafb44d28af
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-07-15 13:26:17 +0500

    dev: clean up the desktop

--- packages/desktop/src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-// Export your library components and utilities here
-// Add exports as you create new components and utilities
-
-// Example:
-// export { default as MyComponent } from './components/MyComponent.svelte';
-// export { myUtility } from './utils/myUtility.ts'; 
--- packages/desktop/src/routes/+page.svelte
+++ packages/desktop/src/routes/+page.svelte
@@ -1,30 +1,22 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { SupaApp } from "@supa/client";
-	
-	// Demo state
-	let electronInfo = {
-		node: '',
-		chrome: '',
-		electron: ''
-	};
-	
-	let clickCount = 0;
-	
-	onMount(() => {
-		// Get version info if available
-		if (typeof process !== 'undefined' && process.versions) {
-			electronInfo = {
-				node: process.versions.node || 'N/A',
-				chrome: process.versions.chrome || 'N/A',
-				electron: process.versions.electron || 'N/A'
-			};
-		}
-	});
+  import { onMount } from "svelte";
+  import { SupaApp } from "@supa/client";
+
+  onMount(() => {
+    if (typeof process !== "undefined" && process.versions) {
+      const info = {
+        node: process.versions.node || "N/A",
+        chrome: process.versions.chrome || "N/A",
+        electron: process.versions.electron || "N/A",
+      };
+
+      console.log("⚛️ Electron info:", info);
+    }
+  });
 </script>
 
 <svelte:head>
-	<title>Supa</title>
+  <title>Supa</title>
 </svelte:head>
 
 <SupaApp />
