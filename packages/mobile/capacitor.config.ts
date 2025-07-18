9600124fd5585c5263520e5a23f5f5508ec5b5ca
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-07-18 11:59:24 +0500

    feat(mobile): add a new pacakge - mobile and setup Capacitor there

---
+++ packages/mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.supago.app',
+  appName: 'Supa',
+  webDir: 'build'
+};
+
+export default config;
