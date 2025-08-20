998123b958392ac51d676f107bb70804446885fc
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-08-20 15:57:23 +0500

    fix(files): remove hover effects from ImageFilePreview for cleaner UX

--- packages/client/src/lib/comps/files/ImageFilePreview.svelte
+++ packages/client/src/lib/comps/files/ImageFilePreview.svelte
@@ -23,7 +23,7 @@
   }
 </script>
 
-<div class="image-preview relative group">
+<div class="image-preview relative inline-block">
   <img
     bind:this={imageElement}
     src={fileInfo.url}
@@ -32,28 +32,4 @@
     onload={handleImageLoad}
     onerror={handleImageError}
   />
-
-  {#if showGallery}
-    <div
-      class="absolute inset-0 bg-black/0 group-hover:bg-black/20 transition-colors rounded flex items-center justify-center opacity-0 group-hover:opacity-100"
-    >
-      <div class="btn-icon bg-white/90">
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <circle cx="11" cy="11" r="8" />
-          <path d="m21 21-4.35-4.35" />
-          <path d="M11 8v6" />
-          <path d="M8 11h6" />
-        </svg>
-      </div>
-    </div>
-  {/if}
 </div>
